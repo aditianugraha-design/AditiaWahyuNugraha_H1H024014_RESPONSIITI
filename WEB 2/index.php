0ee9513590dfcb1974f35e97dae56d3bc6b10fdf
aditianugraha-design <aditia.nugraha@mhs.unsoed.ac.id>
2026-06-10 01:32:56 +0700

    Add files via upload

---
+++ WEB 2/index.php
@@ -0,0 +1,17 @@
+<?php
+$nama = "Aditia Wahyu Nugraha";
+$nim  = "H1H024014";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Web 2</title>
+</head>
+<body>
+<h1>WEB SERVER 2</h1>
+<hr>
+<p>Nama Praktikan: <strong><?= $nama ?></strong></p>
+<p>NIM: <strong><?= $nim ?></strong></p>
+<p>Container: <strong>WEB-2</strong></p>
+</body>
+</html>
